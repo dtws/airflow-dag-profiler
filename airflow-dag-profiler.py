5d69491ca36546d0544d11e84388f1663440212e
Alex Leontiev <nailbiter@dtws-work.in>
2020-12-11 11:35:04 +0900

    commit

--- airflow-dag-profiler.py
+++ airflow-dag-profiler.py
@@ -65,7 +65,7 @@ def airflow_dag_profiler(dag_id, debug, date):
         lines = sql.split("\n")
         start = next(i for i,line in enumerate(lines) if re.match("^ *# property: sql *$",line) is not None)
         end = next(i for i,line in enumerate(lines) if re.match("^ *# property: destination_dataset_table *$",line) is not None)
-        sql = "\n".join(lines[start+2,end-1])
+        sql = "\n".join(lines[start+2:end-1])
         total_bytes_processed = client.query(sql, job_config=bigquery.job.QueryJobConfig(dry_run=True, use_query_cache=False)).total_bytes_processed
         quota.append({"bq_task":bq_task, "total_bytes_processed":total_bytes_processed})
     df = pd.DataFrame(quota)    
