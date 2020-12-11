940aa7c5cf2cd17a53743507109709b4bec18c30
Alex Leontiev <nailbiter@dtws-work.in>
2020-12-11 11:21:35 +0900

    commit

--- airflow-dag-profiler.py
+++ airflow-dag-profiler.py
@@ -25,6 +25,9 @@ import os
 import subprocess
 from collections import namedtuple
 import re
+from tqdm import tqdm
+from google.cloud import bigquery
+import pandas as pd
 
 
 def _add_logger(f):
@@ -48,17 +51,26 @@ def _system(cmd, logger=None):
 @click.command()
 @click.option("--debug/--no-debug", default=False)
 @click.argument("dag_id")
-def airflow_dag_profiler(dag_id, debug):
+@click.argument("date",type=click.DateTime())
+def airflow_dag_profiler(dag_id, debug, date):
     if debug:
         logging.basicConfig(level=logging.INFO)
     tasktree = _system(f"airflow list_tasks -t {dag_id} 2>/dev/null").output
-    #print(f"tasktree: {tasktree}")
+    client = bigquery.Client()
     #<Task(BigQueryOperator): do_filter>
-    tasks = [t for t in re.findall(r"<Task\(BigQueryOperator\): ([a-zA-Z0-9_]+)>",tasktree)]
-    print(f"tasks: {tasks}")
-
-    #
-
+    bq_tasks = [t for t in re.findall(r"<Task\(BigQueryOperator\): ([a-zA-Z0-9_]+)>",tasktree)]
+    quota = []
+    for bq_task in tqdm(bq_tasks):
+        sql = _system(f"airflow render {dag_id} {bq_task} {date.strptime('%Y-%m-%d')}")
+        lines = sql.split("\n")
+        start = next(i for i,line in enumerate(lines) if re.match("^ *# property: sql *$",line) is not None)
+        end = next(i for i,line in enumerate(lines) if re.match("^ *# property: destination_dataset_table *$",line) is not None)
+        sql = "\n".join(lines[start+2,end-1])
+        total_bytes_processed = client.query(sql, job_config=bigquery.job.QueryJobConfig(dry_run=True, use_query_cache=False)).total_bytes_processed
+        quota.append({"bq_task":bq_task, "total_bytes_processed":total_bytes_processed})
+    df = pd.DataFrame(quota)    
+    print(df)
+    print(f"total: {sum(df.total_bytes_processed)} bytes")
 
 if __name__ == "__main__":
     airflow_dag_profiler()
